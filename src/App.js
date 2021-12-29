a45cb64aa53e7540616402b47be945fb745701d7
A <richo_koh@hotmail.com>
2021-12-29 15:16:33 +0800

    refactored into github context

--- src/App.js
+++ src/App.js
@@ -4,23 +4,28 @@ import Navbar from './components/layout/Navbar';
 import About from './pages/About';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import { GithubProvider } from './context/github/GithubContext';
 
 const App = () => {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
-        <main className='mx-auto px-3 container'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/notfound' element={<NotFound />} />
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
+
+          <main className='mx-auto px-3 container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/notfound' element={<NotFound />} />
+              <Route path='/*' element={<NotFound />} />
+            </Routes>
+          </main>
+
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 };
 export default App;
